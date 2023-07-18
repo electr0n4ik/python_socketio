7aa40787242bfeb714afdf32913004e956d37489
elecr0n4ik <andreyshka3@gmail.com>
2023-07-18 17:02:55 +0300

    work on join_room

--- src/socketio_manager.py
+++ src/socketio_manager.py
@@ -15,8 +15,9 @@ class SocketIOManager:
         })
 
         self.clients_count = 1
-        self.rooms = {}
-        self.users = {}
+        self.rooms_dict = {}
+        self.room = None
+        self.users_dict = {}
 
         self.register_events()
 
@@ -26,49 +27,63 @@ class SocketIOManager:
         def connect(sid, environ):
 
             user = User(sid)
-            self.users[sid] = user
+            self.users_dict[sid] = user
             self.clients_count += 1
 
-            self.sio.emit("message", user.id, to=sid)
+            self.sio.emit("connect", user.id, to=sid)
             print(user.id, user.room, user.name, "connected")
 
         @self.sio.event
         def disconnect(sid):
-            user = self.users.get(sid)
+            user = self.users_dict.get(sid)
             if user:
                 user.is_online = False
                 if user.room:
                     self.leave_room(sid)
-                del self.users[sid]
+                del self.users_dict[sid]
 
                 self.clients_count -= 1
 
         @self.sio.on("create_room")
         def create_room(sid, environ):
-            user = self.users.get(sid)
-            room = Room()  # создаем новую комнату
-            room.host = user.session_id  # host
-            self.rooms[room.id] = room
-            user.room = room
-            room.increase_id_counter()
-            self.sio.emit("create_room", {"room_id": room.id, "room_name": room.name}, to=sid)
-            print(sid, user.id, room.id, room.name)
+
+            user = self.users_dict.get(sid)
+
+            self.room = Room()  # создаем новую комнату
+            self.room.host = user.session_id  # назначаем хост комнате
+            self.rooms_dict[self.room.id] = self.room.host  # добавляем комнату в словарь комнат
+            user.room = self.room.host  # сохраняем хост комнаты у пользователя
+
+            self.room.increase_id_counter()  # счетчик класса комнат увеличиваем на 1 после создания очередной комнаты
+
+            self.sio.emit("create_room", {"room_id": self.room.id, "room_name": self.room.name}, to=sid)
+            print("Комната создана", "Имя комнаты - ", self.room.name)
+            print(self.rooms_dict["room"])
 
         @self.sio.on("join_room")
         def join_room(sid, data):
-            room_id = data.get("room_id")
-            print("Ok_join", sid, room_id)
-            # user = self.users.get(sid)
-            # if user:
-            #     room = self.rooms.get(room_id)
-            #     if room:
-            #         user.room = room
-            #         room.members["user"] = user
-            #         self.sio.emit("user_joined", {"room_id": room.id, "user_id": user.id}, room=room.id)
+		  # list_keys = []
+		  # for key_ in self.rooms_dict.keys():
+            #     list_keys.append(key_)
+
+            if data.get("room_id") in self.rooms_dict.keys().get():
+                get_room_id = data.get("room_id")
+                user = self.users_dict.get(sid)
+                if user:
+                    room = self.rooms_dict.get(get_room_id)
+                    if room:
+                        user.room = room
+                        room.add_member(user)
+                        self.sio.emit("message", {"room_id": room.id, "user_id": user.id}, room=room.id)
+                        print(room.id, user.id, room.name, user.name, "joined")
+
+            else:
+                self.sio.emit("message", data={"content": "Не указан ID комната или комнаты не существует!"})
+                print("Error")
 
         @self.sio.on("leave_room")
         def leave_room(sid):
-            user = self.users.get(sid)
+            user = self.users_dict.get(sid)
             if user and user.room:
                 room = user.room
                 user.room = None
